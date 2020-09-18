ef96a480c9faab657ef300aa9fce3c9c95dd1e5d
Karl Söderberg <karl.soderberg@gmail.com>
2020-09-18 21:45:03 +0200

    Initial Commit

---
+++ AppMeatOCRScannerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppMeatOCRScannerApp
+//
+//  Created by Karl Söderberg on 2020-09-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
